ee5f8b06788ca2ab869f18b4b997007cb4d07210
adit-nicholas <adit.nicholas@gmail.com>
2017-02-05 23:51:53 -0500

    Add files via upload

--- createimg.c
+++ createimg.c
@@ -9,18 +9,22 @@
 int main(){
   int r, g , b , i , j ;
   char rgb [100];
-
-  int fd = open("image.ppm", O_WRONLY | O_CREAT | O_EXCL, 0666);
-  write(fd, "P3 \n 700 700 256" , 40);
+  r = 1;
+  g = 2;
+  b = 3;
+  
+  int fd = open("image.ppm", O_WRONLY | O_CREAT | O_EXCL | O_TRUNC, 0644);
+  write(fd, "P3\n700 700 256\n" , 40);
 
   for (i = 0 ; i < 700 ; i++){
     for (j = 0; j < 700 ; j++){
-      r = (r + 1) % 255;
-      g = (g + r) % 255;
-      b = (g + b) % 255;
-    }
-    snprintf(rgb, sizeof(rgb), "%d %d %d \n", r , g , b);
+      r = (r + 1) % 256;
+      g = (g + 2) % 256;
+      b = (3 + b) % 256;
+    
+    snprintf(rgb, sizeof(rgb), "%d %d %d\n", r , g , b);
     write (fd, rgb, sizeof(rgb));
+	}
   }
     return 0;
 }
